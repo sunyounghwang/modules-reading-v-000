bf553c3a731db3fd0abe42cb4048ab0125131def
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-23 00:23:55 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,12 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid
-  include Dance
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+class Dancer
+  extend MetaDancing
 end
